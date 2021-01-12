579e55955d241e17581067f02018b8e5536b7224
Hila Caspi <caspi.home@gmail.com>
2021-01-11 19:52:22 -0800

    upadate server with handlebars

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const path = require("path");
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,10 +12,17 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Setting static files to the public dir
+app.use(express.static(path.join(__dirname, "public")));
+
+// Setting handlebars as the default template engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
