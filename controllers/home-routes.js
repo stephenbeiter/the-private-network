5a9efe608b821b5fb3e69a356827f44b59db416b
Hila Caspi <caspi.home@gmail.com>
2021-01-13 18:31:24 -0800

    add feed page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,23 +4,6 @@ const { Post } = require("../models");
 // index route
 router.get("/", (req, res) => res.render("index", { layout: "landing" }));
 
-// render all posts on homepage
-router.get("/posts", (req, res) => {
-  Post.findAll({
-    attributes: ["id", "username", "email", "profile_img"],
-  })
-    .then((dbPostData) => {
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("homepage", {
-        posts,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
 // signup route
 router.get("/signup", (req, res) => {
   res.render("signup", { layout: "landing" });
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
+const feedRoutes = require("./feed-routes");
 
 router.use("/api", apiRoutes);
 
 // user-facing routes
 router.use("/", homeRoutes);
+router.use("/feed", feedRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
